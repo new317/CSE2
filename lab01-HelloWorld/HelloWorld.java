224f3c0b307d1e4d0b26e3ba35e18dbf57647ebf
new317 <new317@lehigh.edu>
2015-01-23 16:35:23 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,28 @@
+///////////////////////////////////
+//Nelson Westwood
+//Hello World Java Program
+//
+//    First compile the program
+//      javac HelloWorld.java
+//    Run the program
+//      java HelloWorld//
+
+//define a class
+public class HelloWorld {
+    
+    //add a main method
+    public static void main(String[] args) {
+        
+        //print the statement with the box around it
+        System.out.println("#######################");
+        System.out.println("#######################");
+        System.out.println("#                     #");
+        System.out.println("# Hello World 2014!!  #");
+        System.out.println("#                     #");
+        System.out.println("#######################");
+        System.out.println("#######################");
+        System.out.println("test");
+        
+        //test/testtest
+    }
+}
