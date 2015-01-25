f94750e2837a3bd20fbd9bfc40ca0e0b9019883d
new317 <new317@lehigh.edu>
2015-01-25 21:26:45 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,29 @@
+///////////////////////////////////
+//Nelson Westwood
+//Welcome Class Java Program
+//
+//    First compile the program
+//      javac WelcomeClass.java
+//    Run the program
+//      java WelcomeClass//
+
+//define a class
+public class WelcomeClass {
+    
+    //add a main method
+    public static void main(String[] args) {
+        
+        //print the statement with the box around it
+        System.out.println("-----------");
+        System.out.println("| Welcome |");
+        System.out.println("-----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("<-N--E--W--3--1--7->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("This winter break I went skiing. It was sooooo much fun. #skiing #swag");
+        
+        //test/testtest
+    }
+}
