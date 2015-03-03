40fd11b06ca34c08785d84291d0b665ccf07832c
new317 <new317@lehigh.edu>
2015-03-03 01:51:19 +0000

    Updated RunFactorial

--- hw06/RunFactorial.java
+++ hw06/RunFactorial.java
@@ -13,29 +13,29 @@ public static void main (String [] args) {
     Scanner myScanner; //initiate the scanner
     myScanner = new Scanner(System.in); //set the scanner equal to inputs
     
-    int factorial;
-    int number;
-    int total = 1;
+    int factorial; //initiate variable
+    int number; //initiate variable 
+    int total = 1; //initiate variable 
     
-    while (!myScanner.hasNextInt()) {
-            System.out.println("Invalid Input, try again");
-            String trash = myScanner.next();
+    while (!myScanner.hasNextInt()) { //test the next input
+            System.out.println("Invalid Input, try again"); //if it is not an integer print this statement
+            String trash = myScanner.next(); //set the bad input equal to this variable 
         }
         
-    while(myScanner.hasNextInt()) {
-        number = myScanner.nextInt();
-        factorial = number;
-        if (number > 16 || number < 9) {
-            System.out.println("Invalid Input, try again");
+    while(myScanner.hasNextInt()) { //test next input
+        number = myScanner.nextInt(); //if it is an integer, set it equal to number
+        factorial = number; //set factorial equal to number
+        if (number > 16 || number < 9) { //test to make sure number is between 9-16
+            System.out.println("Invalid Input, try again"); //if not print this
         }
         else {
-            System.out.println("Input Accepted ");
-            while (factorial > 0){
-                total = total * factorial;
-                factorial--;
+            System.out.println("Input Accepted "); //if it is between 9-16 print this
+            while (factorial > 0){ //use to decrease factorial by 1 each team (factorial equation)
+                total = total * factorial; //use the total as factorial equation
+                factorial--; //decrement by 1 
             }
-            System.out.println("The facortial " + number + "! = " + total);
-            break;
+            System.out.println("The facortial " + number + "! = " + total); //print the factorial
+            break; //break the while loop
         }
     }
     
